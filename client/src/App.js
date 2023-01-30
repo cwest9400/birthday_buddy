450e809dab419e52501c60b2e5974fae0786cd1d
cwest9400 <chrstphrwest@gmail.com>
2023-01-30 14:17:53 -0500

    added header component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
-
+import Header from "./components/header";
+import './style/cssDev.css'
 // import './App.css';
 
 function App() {
   return (
+    <div>
+    <header>
+      <Header/>
+    </header>
    <h1>hello</h1>
+   </div>
   );
 }
 
---
+++ client/src/components/header.js
@@ -0,0 +1,12 @@
+
+
+export default function Header() {
+    return (
+        <nav>
+            <div>about</div>
+            <div>sign in</div>
+            /
+            <div>sign up</div>
+            </nav>
+    )
+}
