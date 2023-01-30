88655f0d572ec9eb12f9a4260f56146bbb01ad91
cwest9400 <chrstphrwest@gmail.com>
2023-01-30 15:24:08 -0500

    static birthdayCard component created

---
+++ client/src/components/birthdayCard.js
@@ -0,0 +1,13 @@
+export default function BirthdayCard() {
+    
+    return (
+        // bdaycard and date card will have the same styling probably
+        <div className="date-card"> 
+            <p>benjamin lee</p>
+            <p>tuesday, april 4</p>
+            <button>update</button>
+            <button>delete</button>
+
+        </div>
+    )
+}
