4fc8d434bc272c6bb3d766f46b6cdf6929823d52
cwest9400 <chrstphrwest@gmail.com>
2023-01-30 16:39:19 -0500

    connected BE and mappedbirthday cards

--- client/src/components/birthdayCard.js
+++ client/src/components/birthdayCard.js
@@ -1,10 +1,13 @@
-export default function BirthdayCard() {
+export default function BirthdayCard(props) {
+    const fName = props.firstName
+    const lName = props.lastName
+    const birthday = props.birthday
     
     return (
         // bdaycard and date card will have the same styling probably
         <div className="date-card"> 
-            <p>benjamin lee</p>
-            <p>tuesday, april 4</p>
+            <p>{fName} {lName}</p>
+            <p>{birthday}</p>
             <button>update</button>
             <button>delete</button>
 
--- client/src/pages/birthdayBook.js
+++ client/src/pages/birthdayBook.js
@@ -1,8 +1,19 @@
 import { Link } from "react-router-dom"
 import BirthdayCard from "../components/birthdayCard"
+import { useState, useEffect } from "react"
 import "../style/cssDev.css"
 
 export default function BirthdayBook() {
+    const [birthdays, setBirthdays] = useState([])
+    useEffect(() => {
+        fetch("http://127.0.0.1:4000/birthdays")
+        .then((res) => res.json())
+        .then((json) => {
+            setBirthdays(json)
+        })
+        .catch(console.error)
+    }, [])
+console.log(birthdays)
     return (
         <div className="dashboard-container">
             <h1>Dashboard/birthdaybook</h1>
@@ -10,15 +21,20 @@ export default function BirthdayBook() {
             <p>Here you can see upcoming birthdays and add new birthdays.</p>
             <div className="dashboard-nav">
                 <button> add birthday</button>
+                <Link to={'/dashboard'}>
+                <button> back to dashboard</button>
+                </Link>
             </div>
             <h2>up coming birthdays</h2>
             <div className="date-container">
-                <BirthdayCard />
-                <BirthdayCard />
-                <BirthdayCard />
-                <BirthdayCard />
-                <BirthdayCard />
-                <BirthdayCard />
+                {birthdays.map((person)=>{
+                    return (
+                        <BirthdayCard key={person._id} firstName={person.firstName} lastName={person.lastName} birthday={person.birthday} />
+                    )
+                }
+                )}
+                
+                
 
 
 
